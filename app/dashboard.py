a535ccb5f879c96057b4e514cdbb5487588850fa
Yusuf Mert Çelikarslan <yusufmertcelikarslan@gmail.com>
2024-05-13 17:02:48 +0300

    Columns list added.

--- app/dashboard.py
+++ app/dashboard.py
@@ -10,7 +10,7 @@ warnings.filterwarnings("ignore")
 
 st.set_page_config(page_title="Yolcu360 Dashboard", page_icon="🚗", layout="wide")
 
-df_one, df_seven, df_thirty = None, None, None
+df_one, df_seven, df_thirty = None, None, None 
 columns = ["checkInDate", "checkInOffice",	"checkOutOffice",	"brand",	"brand_id",	"model",	"model_id",	"sippCode",	"vendor",	"vendor_id",	"period",	"price_currency",	"price_amount"]
 
 st.title(" :bar_chart: Yolcu360 Dashboard")
@@ -66,93 +66,94 @@ else:
     df = pd.DataFrame([], columns=columns) 
 # arayüzden saat, gün, lokasyon, seçimi yapılacak
 
-# Price Range
-col1, col2 = st.columns((2))
-startPrice = df["price_amount"].min()
-endPrice = df["price_amount"].max()
+if not df.empty:
+    # Price Range
+    col1, col2 = st.columns((2))
+    startPrice = df["price_amount"].min()
+    endPrice = df["price_amount"].max()
 
-with col1:
-    price1 = st.number_input(label="Start Price", min_value=0, max_value=int(endPrice), step=1000)
+    with col1:
+        price1 = st.number_input(label="Start Price", min_value=0, max_value=int(endPrice), step=1000)
 
-with col2:
-    price2 = st.number_input(label="End Price", value=int(endPrice), min_value=int(price1), max_value=int(endPrice)*10, step=1000)
+    with col2:
+        price2 = st.number_input(label="End Price", value=int(endPrice), min_value=int(price1), max_value=int(endPrice)*10, step=1000)
 
-df_filtered = df[(df["price_amount"] >= price1) & (df["price_amount"] <= price2)].copy()
-
-
-
-
-
-#hour = st.sidebar.number_input('Kaç saat sonrasindan baslamak istiyorsunuz? En az 2 saat sonrasını seçmelisiniz.', step=1, min_value=2, max_value=24)
-now = datetime.now()
-date_str = st.sidebar.date_input("Araç kiralayacağınız günü seçiniz?", now, min_value=now)
-hour_str = st.sidebar.time_input("Araç kiralayacağınız saati seçiniz?", value="now", step=900*4)
-#fulldatestr = date_str + "T" + hour_str
-fulldatestr = datetime.combine(date_str, hour_str)
-#day = st.sidebar.number_input('Kaç gün sonrasından baslamak istiyorsunuz? Aynı gün için 0 yazmalısınız.', step=1, min_value=0, max_value=30)
-loc_name = st.sidebar.selectbox('Hangi lokasyon için veri görmek istersiniz?', list(loc_dict.keys()), placeholder="Default")
-filename = st.sidebar.text_input("Dosya Adı", value="yolcu360.xlsx")
-#date_object = datetime.strptime(fulldatestr, "%Y-%m-%dT%H:%M:00")
-date_object = fulldatestr
-hour, day = (date_object.hour - now.hour), (date_object.day - now.day)
-btn = st.sidebar.button('Ara')
-st.sidebar.write(btn)
-
-if btn:
-    st.session_state.clicked_one = True
-    st.session_state.clicked = True
-    df_one, df_seven, df_thirty, df = click_button(hour, day, loc_name, filename, columns)
     df_filtered = df[(df["price_amount"] >= price1) & (df["price_amount"] <= price2)].copy()
 
-st.sidebar.header("Choose your filter: ")
-# Filter vendor
-vendor = st.sidebar.multiselect("Vendor", df_filtered["vendor"].unique())
-if vendor:
-    df_filtered = df_filtered[df_filtered["vendor"].isin(vendor)]
-    if not df_one.empty:
-        df_one = df_one[df_one["vendor"].isin(vendor)]
-    if not df_seven.empty:
-        df_seven = df_seven[df_seven["vendor"].isin(vendor)]
-    if not df_thirty.empty:
-        df_thirty = df_thirty[df_thirty["vendor"].isin(vendor)]
-
-# Filter brand
-brand = st.sidebar.multiselect("Brand", df_filtered["brand"].unique())
-if brand:
-    df_filtered = df_filtered[df_filtered["brand"].isin(brand)]
-    if not df_one.empty:
-        df_one = df_one[df_one["brand"].isin(brand)]
-    if not df_seven.empty:
-        df_seven = df_seven[df_seven["brand"].isin(brand)]
-    if not df_thirty.empty:
-        df_thirty = df_thirty[df_thirty["brand"].isin(brand)]
 
 
-#Dataframe
-tab0, tab1, tab2, tab3 = st.tabs(["Tüm Araçlar", "1 Günlük", "7 Günlük", "30 Günlük"])
-
-with tab0:
-    st.write("Tüm Araçlar")
-    st.dataframe(df_filtered)
-
-with tab1:
-    st.write("1 Günlük")
-    st.dataframe(df_one)
 
 
-with tab2:
-    st.write("7 Günlük")
-    st.dataframe(df_seven)
+    #hour = st.sidebar.number_input('Kaç saat sonrasindan baslamak istiyorsunuz? En az 2 saat sonrasını seçmelisiniz.', step=1, min_value=2, max_value=24)
+    now = datetime.now()
+    date_str = st.sidebar.date_input("Araç kiralayacağınız günü seçiniz?", now, min_value=now)
+    hour_str = st.sidebar.time_input("Araç kiralayacağınız saati seçiniz?", value="now", step=900*4)
+    #fulldatestr = date_str + "T" + hour_str
+    fulldatestr = datetime.combine(date_str, hour_str)
+    #day = st.sidebar.number_input('Kaç gün sonrasından baslamak istiyorsunuz? Aynı gün için 0 yazmalısınız.', step=1, min_value=0, max_value=30)
+    loc_name = st.sidebar.selectbox('Hangi lokasyon için veri görmek istersiniz?', list(loc_dict.keys()), placeholder="Default")
+    filename = st.sidebar.text_input("Dosya Adı", value="yolcu360.xlsx")
+    #date_object = datetime.strptime(fulldatestr, "%Y-%m-%dT%H:%M:00")
+    date_object = fulldatestr
+    hour, day = (date_object.hour - now.hour), (date_object.day - now.day)
+    btn = st.sidebar.button('Ara')
+    st.sidebar.write(btn)
+
+    if btn:
+        st.session_state.clicked_one = True
+        st.session_state.clicked = True
+        df_one, df_seven, df_thirty, df = click_button(hour, day, loc_name, filename, columns)
+        df_filtered = df[(df["price_amount"] >= price1) & (df["price_amount"] <= price2)].copy()
+
+    st.sidebar.header("Choose your filter: ")
+    # Filter vendor
+    vendor = st.sidebar.multiselect("Vendor", df_filtered["vendor"].unique())
+    if vendor:
+        df_filtered = df_filtered[df_filtered["vendor"].isin(vendor)]
+        if not df_one.empty:
+            df_one = df_one[df_one["vendor"].isin(vendor)]
+        if not df_seven.empty:
+            df_seven = df_seven[df_seven["vendor"].isin(vendor)]
+        if not df_thirty.empty:
+            df_thirty = df_thirty[df_thirty["vendor"].isin(vendor)]
+
+    # Filter brand
+    brand = st.sidebar.multiselect("Brand", df_filtered["brand"].unique())
+    if brand:
+        df_filtered = df_filtered[df_filtered["brand"].isin(brand)]
+        if not df_one.empty:
+            df_one = df_one[df_one["brand"].isin(brand)]
+        if not df_seven.empty:
+            df_seven = df_seven[df_seven["brand"].isin(brand)]
+        if not df_thirty.empty:
+            df_thirty = df_thirty[df_thirty["brand"].isin(brand)]
 
 
-with tab3:
-    st.write("30 Günlük")
-    st.dataframe(df_thirty)
+    #Dataframe
+    tab0, tab1, tab2, tab3 = st.tabs(["Tüm Araçlar", "1 Günlük", "7 Günlük", "30 Günlük"])
+
+    with tab0:
+        st.write("Tüm Araçlar")
+        st.dataframe(df_filtered)
+
+    with tab1:
+        st.write("1 Günlük")
+        st.dataframe(df_one)
 
 
-#Graphs
-st.header("Graphs")
-day_interval = st.selectbox("Kaç Günlük Kiralama?", [1,7,30], key="graph")
-df_filtered_graph = df_filtered[df_filtered["period"] == day_interval]
-fig = px.bar(df_filtered_graph, x='vendor', y='price_amount', color='brand', text="sippCode", barmode='group', title="Price Amount by Vendor and Brand")
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    with tab2:
+        st.write("7 Günlük")
+        st.dataframe(df_seven)
+
+
+    with tab3:
+        st.write("30 Günlük")
+        st.dataframe(df_thirty)
+
+
+    #Graphs
+    st.header("Graphs")
+    day_interval = st.selectbox("Kaç Günlük Kiralama?", [1,7,30], key="graph")
+    df_filtered_graph = df_filtered[df_filtered["period"] == day_interval]
+    fig = px.bar(df_filtered_graph, x='vendor', y='price_amount', color='brand', text="sippCode", barmode='group', title="Price Amount by Vendor and Brand")
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
